d912eaa5f88be0cd0ae11266281a99733dcedeff
Benjamin Ang <ben.ang@fastmail.com>
2023-06-23 09:00:28 -0400

    Add shadcn components
    Had to add a patch to get tailwindcss-animate to work ok in
    tailwind.config.ts.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,24 @@
 import type { Config } from 'tailwindcss'
 import defaultTheme from 'tailwindcss/defaultTheme.js'
 import tailwindcssRadix from 'tailwindcss-radix'
+import tailwindcssAnimate from 'tailwindcss-animate'
 
 export default {
 	content: ['./app/**/*.{ts,tsx,jsx,js}'],
 	darkMode: 'class',
 	theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+          "2xl": "1400px",
+      },
+    },
 		extend: {
 			colors: {
+        border: "hsl(var(--color-border))",
+        input: "hsl(var(--color-input))",
+        ring: "hsl(var(--color-ring))",
 				background: 'hsl(var(--color-background))',
 				foreground: 'hsl(var(--color-foreground))',
 				brand: {
@@ -22,6 +33,34 @@ export default {
 					tertiary: 'hsl(var(--color-brand-tertiary))',
 				},
 				danger: 'hsl(var(--color-danger))',
+        primary: {
+          DEFAULT: "hsl(var(--color-primary))",
+          foreground: "hsl(var(--color-primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--color-secondary))",
+          foreground: "hsl(var(--color-secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--color-destructive))",
+          foreground: "hsl(var(--color-destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--color-muted))",
+          foreground: "hsl(var(--color-muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--color-accent))",
+          foreground: "hsl(var(--color-accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--color-popover))",
+          foreground: "hsl(var(--color-popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--color-card))",
+          foreground: "hsl(var(--color-card-foreground))",
+        },
 				day: {
 					100: 'hsl(var(--color-day-100))',
 					200: 'hsl(var(--color-day-200))',
@@ -41,6 +80,11 @@ export default {
 					700: 'hsl(var(--color-night-700))',
 				},
 			},
+      borderRadius: {
+        lg: `var(--radius)`,
+        md: `calc(var(--radius) - 2px)`,
+        sm: "calc(var(--radius) - 4px)",
+      },
 			fontFamily: {
 				sans: [
 					'Nunito Sans',
@@ -48,7 +92,21 @@ export default {
 					...defaultTheme.fontFamily.sans,
 				],
 			},
-			fontSize: {
+      keyframes: {
+        "accordion-down": {
+          from: { height: '0' },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: '0' },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+      fontSize: {
 				// 1rem = 16px
 				/** 80px size / 84px high / bold */
 				mega: ['5rem', { lineHeight: '5.25rem', fontWeight: '700' }],
@@ -87,5 +145,5 @@ export default {
 			},
 		},
 	},
-	plugins: [tailwindcssRadix],
+	plugins: [tailwindcssRadix, tailwindcssAnimate],
 } satisfies Config
