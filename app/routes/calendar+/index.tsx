b22235eea54efd5cd735c6c945beff53b938cf3b
Benjamin Ang <ben.ang@fastmail.com>
2023-06-28 16:15:45 -0400

    Revert "Change format of date-fns import statement"
    This reverts commit 360ab272c6bf9b07a185a8588646c3ac114981d5.

--- app/routes/calendar+/index.tsx
+++ app/routes/calendar+/index.tsx
@@ -1,7 +1,10 @@
 import { Form, json , useLoaderData , useActionData } from '~/remix.ts';
 import type { ActionArgs , LoaderArgs } from '~/remix.ts'
 import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
-import { format, parse, startOfWeek, getDay } from 'date-fns'
+import format from 'date-fns/format'
+import parse from 'date-fns/parse'
+import startOfWeek from 'date-fns/startOfWeek'
+import getDay from 'date-fns/getDay'
 import enUS from 'date-fns/locale/en-US'
 import "react-big-calendar/lib/css/react-big-calendar.css";
 
