431f54c15e0ff494bdeda5b81d6bfd5d330a79e3
Parker <parkied@gmail.com>
2023-07-19 11:42:35 -0700

    add whitespace

--- app/routes/calendar+/index.tsx
+++ app/routes/calendar+/index.tsx
@@ -342,7 +342,7 @@ function RegistrationDialogue({ selectedEventId, events }: RegistrationProps) {
 					defaultValue={volunteerTypes[volunteerTypeIdx].field}
 					disabled={isRegistered}
 				>
-					<ul className="">
+					<ul className="pb-4">
 						{volunteerTypes.map(volunteerType => {
 							const spotsLeft =
 								calEvent[`${volunteerType.field}Req`] -
@@ -399,7 +399,7 @@ function RegistrationDialogue({ selectedEventId, events }: RegistrationProps) {
 				</RadioGroup>
 				{!isRegistered ? null : (
 					<>
-						<div>
+						<div className="pb-4">
 							<input type="hidden" name="role" value={registeredAs.field} />
 							You are registered to volunteer in this event as one of the{' '}
 							{registeredAs.displayName}.
