8518ca3a5fde580704732c3866a81997a07d44cd
Parker <parkied@gmail.com>
2023-09-29 08:42:07 -0700

    Remove try/catch, other minor tweaks

--- app/components/ui/calendar.tsx
+++ app/components/ui/calendar.tsx
@@ -3,7 +3,7 @@ import { ChevronLeft, ChevronRight } from "lucide-react"
 import { DayPicker } from "react-day-picker"
 
 import { cn } from "~/utils/misc.ts"
-import { buttonVariants } from "~/components/ui/button"
+import { buttonVariants } from "~/components/ui/button.tsx"
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>
 
