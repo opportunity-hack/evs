17698679b70e4458a29bb3681225177f3d9f6825
Greg V <greg.vannoni@gmail.com>
2023-12-02 16:38:05 -0700

    Test update for #59

--- tests/e2e/onboarding.test.ts
+++ tests/e2e/onboarding.test.ts
@@ -8,7 +8,7 @@ import {
 	setSignupPassword,
 } from '../playwright-utils.ts'
 import { readEmail } from '../mocks/utils.ts'
-import { siteEmailAddress } from '~/data.ts'
+import { siteEmailAddressWithName } from '~/data.ts'
 
 const urlRegex = /(?<url>https?:\/\/[^\s$.?#].[^\s]*)/
 function extractUrl(text: string) {
@@ -63,7 +63,7 @@ test('onboarding with link', async ({ page }) => {
 	const email = await readEmail(onboardingData.email)
 	invariant(email, 'Email not found')
 	expect(email.to).toBe(onboardingData.email)
-	expect(email.from).toBe(siteEmailAddress)
+	expect(email.from).toBe(siteEmailAddressWithName)
 	expect(email.subject).toMatch(/welcome/i)
 	const onboardingUrl = extractUrl(email.text)
 	invariant(onboardingUrl, 'Onboarding URL not found')
@@ -139,7 +139,7 @@ test('onboarding with a short code', async ({ page }) => {
 	const email = await readEmail(onboardingData.email)
 	invariant(email, 'Email not found')
 	expect(email.to).toBe(onboardingData.email)
-	expect(email.from).toBe(siteEmailAddress)
+	expect(email.from).toBe(siteEmailAddressWithName)
 	expect(email.subject).toMatch(/welcome/i)
 	const codeMatch = email.text.match(
 		/Here's your verification code: (?<code>\d+)/,
@@ -188,7 +188,7 @@ test('reset password with a link', async ({ page }) => {
 	invariant(email, 'Email not found')
 	expect(email.subject).toMatch(/password reset/i)
 	expect(email.to).toBe(user.email)
-	expect(email.from).toBe(siteEmailAddress)
+	expect(email.from).toBe(siteEmailAddressWithName)
 	const resetPasswordUrl = extractUrl(email.text)
 	invariant(resetPasswordUrl, 'Reset password URL not found')
 	await page.goto(resetPasswordUrl)
@@ -239,7 +239,7 @@ test('reset password with a short code', async ({ page }) => {
 	invariant(email, 'Email not found')
 	expect(email.subject).toMatch(/password reset/i)
 	expect(email.to).toBe(user.email)
-	expect(email.from).toBe(siteEmailAddress)
+	expect(email.from).toBe(siteEmailAddressWithName)
 	const codeMatch = email.text.match(
 		/Here's your verification code: (?<code>\d+)/,
 	)
