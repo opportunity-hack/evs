d2e6211be7b72cb8d6ddd9a16ae81e9050fb8313
Greg V <greg.vannoni@gmail.com>
2023-12-22 12:21:26 -0700

    Spruce up email a tad

--- app/routes/admin+/_email+/CustomEmail.server.tsx
+++ app/routes/admin+/_email+/CustomEmail.server.tsx
@@ -1,20 +1,22 @@
 import { Container, Html, Link, Tailwind, Text } from '@react-email/components'
 import tailwindConfig from '../../../../tailwind.config.ts'
-import { siteBaseUrl } from '~/data.ts'
+import { siteBaseUrl,  siteName } from '~/data.ts'
 
 export function CustomEmail({ message }: { message: string }) {
 	return (
 		<Tailwind config={tailwindConfig}>
 			<Html lang="en" dir="ltr">
-				<Container>
-					<h1>
-						<Text>A Message from The Barn</Text>
+				<Container style={{ backgroundColor: '#F8F8F8', padding: '20px', borderRadius: '5px' }}>
+					<h1 style={{ color: '#333', fontSize: '24px', marginBottom: '20px' }}>
+						<Text>A Message from {siteName}</Text>
 					</h1>
-					<p>
+					<p style={{ color: '#555', fontSize: '16px', marginBottom: '10px' }}>
 						<Text>{message}</Text>
 					</p>
-					<p>
-						<Link href={siteBaseUrl}>View upcoming events</Link>
+					<p style={{ color: '#555', fontSize: '16px', marginBottom: '10px' }}>
+						<Link href={siteBaseUrl + "/calendar"} style={{ color: '#007BFF', textDecoration: 'none' }}>
+							View upcoming events
+						</Link>
 					</p>
 				</Container>
 			</Html>
