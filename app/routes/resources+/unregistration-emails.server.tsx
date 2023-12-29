813a0ee4f1fda789722dee6d5994e8d241d29c9c
Greg V <greg.vannoni@gmail.com>
2023-12-29 11:18:09 -0700

    #67 send email to peeps when unregister

---
+++ app/routes/resources+/unregistration-emails.server.tsx
@@ -0,0 +1,44 @@
+import type { Event } from '@prisma/client'
+import { Container, Html, Tailwind, Text } from '@react-email/components'
+import tailwindConfig from '~/../tailwind.config.ts'
+import { format } from 'date-fns'
+import { volunteerTypes } from '~/data.ts'
+
+export function UnregistrationEmail({
+	event,
+	role,
+}: {
+	event: Event
+	role: 'cleaningCrew' | 'lessonAssistants' | 'sideWalkers' | 'horseLeaders'
+}) {
+	let roleName
+	for (let v of volunteerTypes) {
+		if (v.field == role) {
+			roleName = v.displayName
+		}
+	}
+
+	return (
+		<Tailwind config={tailwindConfig}>
+			<Html lang="en" dir="ltr">
+				<Container>
+					<h1>
+						<Text>Thanks for considering volunteering! We hope to see you again!</Text>
+					</h1>
+					<p>
+						<Text>
+							You've unregistered to volunteer{' '}
+							{roleName ? `as one of the ${roleName}` : ''} to help with the
+							following event:
+						</Text>
+						<Text>{event.title}</Text>
+						<Text>On: {format(event.start, 'MMMM do, y')}</Text>
+						<Text>
+							From: {format(event.start, 'p')} - {format(event.end, 'p')}
+						</Text>
+					</p>
+				</Container>
+			</Html>
+		</Tailwind>
+	)
+}
